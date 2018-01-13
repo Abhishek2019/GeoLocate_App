81a9c0e024a1f11893b900f69d97b918b60ba996
Abhishek2019 <abhishekw13@gmail.com>
2018-01-14 00:44:04 +0530

    Marker Added 14/1/18

--- GeoLocate_main/App.js
+++ GeoLocate_main/App.js
@@ -16,14 +16,22 @@ class App extends Component{
                       latitude: 37.78825,
                       longitude: -122.4324,
                       latitudeDelta: 0.0922,
-                      longitudeDelta: 0.0421,
+                      longitudeDelta: 0.0421
                   }}
 
                   style={styles.MapViewStyles}
+              >
 
+                  <MapView.Marker
 
+                      coordinate={{
+                          latitude: 37.78825,
+                          longitude: -122.4324
+                      }}
+                  />
 
-              />
+              </MapView>
+
           </View>
 
       );
